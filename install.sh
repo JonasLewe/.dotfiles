d3c24ecbbe6c70a195d45a5c2fb51d6d8fd25d75
Jonas Lewe <j.lewe@gmx.net>
2022-08-13 13:05:12 +0200

    add X11 config for arch

--- install.sh
+++ install.sh
@@ -13,32 +13,32 @@ fi
 #######
 # zsh #
 #######
-mkdir -p "$XDG_CONFIG_HOME/zsh"
-ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
-ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
-ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
+sudo mkdir -p "$XDG_CONFIG_HOME/zsh"
+sudo ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
+sudo ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
+sudo ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 
-rm -rf "$XDG_CONFIG_HOME/zsh/external"
-ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
+sudo rm -rf "$XDG_CONFIG_HOME/zsh/external"
+sudo ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 
 ########
 # nvim #
 ########
-mkdir -p "$XDG_CONFIG_HOME/nvim"
-mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
+sudo mkdir -p "$XDG_CONFIG_HOME/nvim"
+sudo mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
 
-ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
+sudo ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
 
 # Install vim plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+sudo sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # :PlugInstall
-nvim --headless +PlugInstall +qall
-nvim +UpdateRemotePlugins +qa
+sudo nvim --headless +PlugInstall +qall
+sudo nvim +UpdateRemotePlugins +qa
 
 ########
 # tmux #
 ########
-mkdir -p "$XDG_CONFIG_HOME/tmux"
-ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
+sudo mkdir -p "$XDG_CONFIG_HOME/tmux"
+sudo ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
