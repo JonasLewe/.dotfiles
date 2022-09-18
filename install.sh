419648eaccb1a3b80c6cad884636855117c8ee0f
Jonas Lewe <j.lewe@gmx.net>
2022-09-18 19:51:23 +1000

    edit

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 ###########
 if [ -f /etc/lsb-release ]; then
     # Ubuntu
-    sudo apt install zsh tmux nvim ripgrep -y
+    sudo apt install zsh tmux nvim ripgrep
 else
     exit 0
 fi
@@ -15,23 +15,23 @@ source "$HOME/.dotfiles/zsh/.zshenv"
 #######
 # zsh #
 #######
-rm -rf "$XDG_CONFIG_HOME/zsh"
-mkdir -p "$XDG_CONFIG_HOME/zsh"
-ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
-ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
-ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
-
-rm -rf "$XDG_CONFIG_HOME/zsh/external"
-ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
+sudo rm -rf "$XDG_CONFIG_HOME/zsh"
+sudo mkdir -p "$XDG_CONFIG_HOME/zsh"
+sudo ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
+sudo ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
+sudo ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
+ 
+sudo rm -rf "$XDG_CONFIG_HOME/zsh/external"
+sudo ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 
 ########
 # nvim #
 ########
-rm -rf "$XDG_CONFIG_HOME/nvim"
-mkdir -p "$XDG_CONFIG_HOME/nvim"
-mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
+sudo rm -rf "$XDG_CONFIG_HOME/nvim"
+sudo mkdir -p "$XDG_CONFIG_HOME/nvim"
+sudo mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
 
-ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
+sudo ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
 
 # Install vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
@@ -50,9 +50,9 @@ git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerl
 ########
 # tmux #
 ########
-rm -rf "$XDG_CONFIG_HOME/tmux"
-mkdir -p "$XDG_CONFIG_HOME/tmux"
-ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
+sudo rm -rf "$XDG_CONFIG_HOME/tmux"
+sudo mkdir -p "$XDG_CONFIG_HOME/tmux"
+sudo ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
 
 # Install tpm 
 git clone https://github.com/tmux-plugins/tpm $XDG_CONFIG_HOME/tmux/plugins/tpm
