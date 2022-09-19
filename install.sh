088c6c467f22454fd50d96e58a4c47cf4382e74e
Jonas Lewe <j.lewe@gmx.net>
2022-09-19 22:21:12 +1000

    update

--- install.sh
+++ install.sh
@@ -44,13 +44,14 @@ mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
 
 ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
 
-# Install vim plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# install neovim plugin manager
+[ ! -f "$DOTFILES/nvim/autoload/plug.vim" ] && curl -fLo "$DOTFILES/nvim/autoload/plug.vim" --create -dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# :PlugInstall
-nvim --headless +PlugInstall +qall
-nvim +UpdateRemotePlugins +qa
+mkdir -p "$XDG_CONFIG_HOME/nvim/autoload"
+ln -sf "$DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
+
+# Install (or update) all the plugins
+nvim --noplugin +PlugUpdate +qa
 
 ########
 # tmux #
