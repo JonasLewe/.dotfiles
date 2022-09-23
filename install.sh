7e10307348187074135a13221816752b07c45074
Jonas Lewe <j.lewe@gmx.net>
2022-09-23 14:31:21 +1000

    fix typo

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ sudo ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
 [ ! -f "$DOTFILES/nvim/autoload/plug.vim" ] && curl -fLo "$DOTFILES/nvim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p "$XDG_CONFIG_HOME/nvim/autoload"
-ln -sf $DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
+ln -sf "$DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
 
 # Install (or update) all the plugins
 nvim --noplugin +PlugUpdate +qa
