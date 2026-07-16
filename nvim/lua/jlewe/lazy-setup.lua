03cec4a70821c24fc98a8c2d18b7e677d529c2b7
Jonas Lewe <jonas.lewe@icloud.com>
2026-07-16 23:15:12 +0200

    Format remaining Neovim configuration

--- nvim/lua/jlewe/lazy-setup.lua
+++ nvim/lua/jlewe/lazy-setup.lua
@@ -25,9 +25,12 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 -- Bootstrap: if lazy.nvim is not installed yet, clone it
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git", "clone", "--filter=blob:none",
+    "git",
+    "clone",
+    "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
+    "--branch=stable",
+    lazypath,
   })
 end
 
--- nvim/lua/jlewe/plugins/dap.lua
+++ nvim/lua/jlewe/plugins/dap.lua
@@ -62,16 +62,76 @@ return {
     },
     -- Lazy-load: plugin loads only when you press a debug keybind
     keys = {
-      { "<leader>db", function() require("dap").toggle_breakpoint() end,                                    desc = "DAP: Toggle breakpoint" },
-      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "DAP: Conditional breakpoint" },
-      { "<leader>dc", function() require("dap").continue() end,                                             desc = "DAP: Start / Continue" },
-      { "<leader>di", function() require("dap").step_into() end,                                            desc = "DAP: Step into" },
-      { "<leader>do", function() require("dap").step_over() end,                                            desc = "DAP: Step over" },
-      { "<leader>dO", function() require("dap").step_out() end,                                             desc = "DAP: Step out" },
-      { "<leader>dr", function() require("dap").repl.toggle() end,                                          desc = "DAP: Toggle REPL" },
-      { "<leader>dl", function() require("dap").run_last() end,                                             desc = "DAP: Run last config" },
-      { "<leader>dt", function() require("dap").terminate() end,                                            desc = "DAP: Terminate session" },
-      { "<leader>du", function() require("dapui").toggle() end,                                             desc = "DAP: Toggle UI" },
+      {
+        "<leader>db",
+        function()
+          require("dap").toggle_breakpoint()
+        end,
+        desc = "DAP: Toggle breakpoint",
+      },
+      {
+        "<leader>dB",
+        function()
+          require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+        end,
+        desc = "DAP: Conditional breakpoint",
+      },
+      {
+        "<leader>dc",
+        function()
+          require("dap").continue()
+        end,
+        desc = "DAP: Start / Continue",
+      },
+      {
+        "<leader>di",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "DAP: Step into",
+      },
+      {
+        "<leader>do",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "DAP: Step over",
+      },
+      {
+        "<leader>dO",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "DAP: Step out",
+      },
+      {
+        "<leader>dr",
+        function()
+          require("dap").repl.toggle()
+        end,
+        desc = "DAP: Toggle REPL",
+      },
+      {
+        "<leader>dl",
+        function()
+          require("dap").run_last()
+        end,
+        desc = "DAP: Run last config",
+      },
+      {
+        "<leader>dt",
+        function()
+          require("dap").terminate()
+        end,
+        desc = "DAP: Terminate session",
+      },
+      {
+        "<leader>du",
+        function()
+          require("dapui").toggle()
+        end,
+        desc = "DAP: Toggle UI",
+      },
     },
     config = function()
       local dap = require("dap")
@@ -95,10 +155,10 @@ return {
       end
 
       -- Breakpoint signs in the gutter
-      vim.fn.sign_define("DapBreakpoint",          { text = "●", texthl = "DiagnosticError" })
-      vim.fn.sign_define("DapBreakpointCondition",  { text = "◆", texthl = "DiagnosticWarn" })
-      vim.fn.sign_define("DapBreakpointRejected",   { text = "○", texthl = "DiagnosticHint" })
-      vim.fn.sign_define("DapStopped",              { text = "▶", texthl = "DiagnosticInfo", linehl = "CursorLine" })
+      vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DiagnosticError" })
+      vim.fn.sign_define("DapBreakpointCondition", { text = "◆", texthl = "DiagnosticWarn" })
+      vim.fn.sign_define("DapBreakpointRejected", { text = "○", texthl = "DiagnosticHint" })
+      vim.fn.sign_define("DapStopped", { text = "▶", texthl = "DiagnosticInfo", linehl = "CursorLine" })
     end,
   },
 }
