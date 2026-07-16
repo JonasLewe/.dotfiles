caa6c52895d916e5be4bd336101b6659b7f7a828
Jonas Lewe <jonas.lewe@ibm.com>
2026-07-16 20:29:04 +0200

    Show branch divergence in LazyGit

--- nvim/lua/jlewe/plugins/lazygit.lua
+++ nvim/lua/jlewe/plugins/lazygit.lua
@@ -13,6 +13,10 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
+    init = function()
+        vim.g.lazygit_use_custom_config_file_path = 1
+        vim.g.lazygit_config_file_path = vim.fn.stdpath("config") .. "/lazygit.yml"
+    end,
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
